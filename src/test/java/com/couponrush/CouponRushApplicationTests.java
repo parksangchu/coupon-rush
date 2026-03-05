23256165a029ee1f6a478220c5a73cfe68021014
scpark <didi1484@gmail.com>
2026-03-05 14:37:19 +0900

    infra: 스프링 부트 실행 환경과 로컬 MySQL 컴포즈 구성 추가

---
+++ src/test/java/com/couponrush/CouponRushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponrush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponRushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
