361c6c65c66d8fa2efcb43099669eadd72dcb672
scpark <didi1484@gmail.com>
2026-03-17 09:10:37 +0900

    test: Pessimistic Lock 동시성 테스트 추가
    150스레드 동시 발급 시 초과 발급 없음, 중복 발급 예외, 수량 소진 예외 검증.
    IntegrationTestBase로 Testcontainers 설정 공통화.

--- src/test/java/com/couponrush/IntegrationTestBase.java
+++ src/test/java/com/couponrush/IntegrationTestBase.java
@@ -1,22 +1,16 @@
 package com.couponrush;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.MySQLContainer;
 
 @SpringBootTest
-class CouponRushApplicationTests {
+public abstract class IntegrationTestBase {
 
-	@ServiceConnection
-	static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.4");
+    @ServiceConnection
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.4");
 
-	static {
-		mysql.start();
-	}
-
-	@Test
-	void contextLoads() {
-	}
-
+    static {
+        mysql.start();
+    }
 }
