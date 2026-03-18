1032f3304e7697015716427e91f58d286ae5f826
scpark <didi1484@gmail.com>
2026-03-18 10:57:15 +0900

    infra: EC2 EBS 볼륨 20GB → 30GB로 상향
    AL2023 AMI 스냅샷이 30GB를 요구해서 20GB로는 인스턴스 생성이 실패한다.

--- terraform/ec2-test.tf
+++ terraform/ec2-test.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "test" {
   vpc_security_group_ids = [aws_security_group.test.id]
 
   root_block_device {
-    volume_size = 20
+    volume_size = 30
     volume_type = "gp3"
   }
 
