cf1365cbc31d021997891a0f0880933ee2c4c096
scpark <didi1484@gmail.com>
2026-03-23 20:22:59 +0900

    refactor: 미사용 countByCouponId 메서드 제거

--- src/main/java/com/couponrush/domain/coupon/repository/IssuanceRepository.java
+++ src/main/java/com/couponrush/domain/coupon/repository/IssuanceRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IssuanceRepository extends JpaRepository<Issuance, Long> {
     boolean existsByCouponIdAndUserId(Long couponId, Long userId);
-    long countByCouponId(Long couponId);
 }
